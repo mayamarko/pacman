0d135b108c5519f1a660b2a3606eba8e5615dc8c
mayamarko <mayamark@post.bgu.ac.il>
2019-04-21 20:01:02 +0300

    initial commit

---
+++ js/game.js
@@ -0,0 +1,255 @@
+var context = canvas.getContext("2d");
+var shape = new Object();
+var board;
+var score;
+var pac_color;
+var start_time;
+var time_elapsed;
+var interval;
+
+
+// toggle_visibility("gameArea", "welcome");
+// toggle_visibility("gameArea","about");
+
+setInvisibale("gameArea");
+setInvisibale("about");
+//Start();
+
+
+function setInvisibale(div){
+    document.getElementById(div).style.display = "none";
+}
+function setVisibale(div){
+    document.getElementById(div).style.display = "block";
+}
+
+// Get the modal
+var modal = document.getElementById('signinModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("sighin");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+$(document).ready(function(){
+    $("#singupForm").validate({
+        rules:{
+            username='required',
+            date='required',
+            first_name:{
+                required=true,
+            },
+            last_name:{
+                required=true,
+            },
+            email:{
+                required=true,
+                email=true,
+            },
+            password:{
+                required=true,
+                minlength:5,
+            }
+
+        }
+    })
+}
+// $("#welcomeing").click(function(){      
+//     $("#welcome").toggle();              
+// });
+
+// $("#abouting").click(function(){      
+//    setVisibale("about");
+//    setInvisibale("wrapper");
+// });
+
+// function showwelcome(){
+//     document.getElementById("wrapper").style.display = "block";
+//     document.getElementById("about").style.display = "none";
+// }
+
+// function showabout(){
+//     document.getElementById("about").style.display = "block";
+//     document.getElementById("wrapper").style.display = "none";
+// }
+// function toggle_visibility(id1, id2) {  
+//     var e = document.getElementById(id1);
+//     var e2 = document.getElementById(id2);
+//     if(e.style.display == 'block') {                
+//        e.style.display = 'block';             
+//        e2.style.display = 'none';
+//     }
+//     else {
+//        e.style.display = 'none';            
+//        e2.style.display = 'block';
+//     }              
+//  } 
+
+function Start() {
+    board = new Array();
+    score = 0;
+    pac_color = "yellow";
+    var cnt = 100;
+    var food_remain = 50;
+    var pacman_remain = 1;
+    start_time = new Date();
+    for (var i = 0; i < 10; i++) {
+        board[i] = new Array();
+        //put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
+        for (var j = 0; j < 10; j++) {
+            if ((i === 3 && j === 3) || (i === 3 && j === 4) || (i === 3 && j === 5) || (i === 6 && j === 1) || (i === 6 && j === 2)) {
+                board[i][j] = 4;
+            } else {
+                var randomNum = Math.random();
+                if (randomNum <= 1.0 * food_remain / cnt) {
+                    food_remain--;
+                    board[i][j] = 1;
+                } else if (randomNum < 1.0 * (pacman_remain + food_remain) / cnt) {
+                    shape.i = i;
+                    shape.j = j;
+                    pacman_remain--;
+                    board[i][j] = 2;
+                } else {
+                    board[i][j] = 0;
+                }
+                cnt--;
+            }
+        }
+    }
+    while (food_remain > 0) {
+        var emptyCell = findRandomEmptyCell(board);
+        board[emptyCell[0]][emptyCell[1]] = 1;
+        food_remain--;
+    }
+    keysDown = {};
+    addEventListener("keydown", function (e) {
+        keysDown[e.code] = true;
+    }, false);
+    addEventListener("keyup", function (e) {
+        keysDown[e.code] = false;
+    }, false);
+    interval = setInterval(UpdatePosition, 250);
+}
+
+
+function findRandomEmptyCell(board) {
+    var i = Math.floor((Math.random() * 9) + 1);
+    var j = Math.floor((Math.random() * 9) + 1);
+    while (board[i][j] !== 0) {
+        i = Math.floor((Math.random() * 9) + 1);
+        j = Math.floor((Math.random() * 9) + 1);
+    }
+    return [i, j];
+}
+
+/**
+ * @return {number}
+ */
+function GetKeyPressed() {
+    if (keysDown['ArrowUp']) {
+        return 1;
+    }
+    if (keysDown['ArrowDown']) {
+        return 2;
+    }
+    if (keysDown['ArrowLeft']) {
+        return 3;
+    }
+    if (keysDown['ArrowRight']) {
+        return 4;
+    }
+}
+
+function Draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height); //clean board
+    lblScore.value = score;
+    lblTime.value = time_elapsed;
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            var center = new Object();
+            center.x = i * 60 + 30;
+            center.y = j * 60 + 30;
+            if (board[i][j] === 2) {
+                context.beginPath();
+                context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
+                context.lineTo(center.x, center.y);
+                context.fillStyle = pac_color; //color
+                context.fill();
+                context.beginPath();
+                context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
+                context.fillStyle = "black"; //color
+                context.fill();
+            } else if (board[i][j] === 1) {
+                context.beginPath();
+                context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
+                context.fillStyle = "black"; //color
+                context.fill();
+            } else if (board[i][j] === 4) {
+                context.beginPath();
+                context.rect(center.x - 30, center.y - 30, 60, 60);
+                context.fillStyle = "grey"; //color
+                context.fill();
+            }
+        }
+    }
+
+
+}
+
+function UpdatePosition() {
+    board[shape.i][shape.j] = 0;
+    var x = GetKeyPressed();
+    if (x === 1) {
+        if (shape.j > 0 && board[shape.i][shape.j - 1] !== 4) {
+            shape.j--;
+        }
+    }
+    if (x === 2) {
+        if (shape.j < 9 && board[shape.i][shape.j + 1] !== 4) {
+            shape.j++;
+        }
+    }
+    if (x === 3) {
+        if (shape.i > 0 && board[shape.i - 1][shape.j] !== 4) {
+            shape.i--;
+        }
+    }
+    if (x === 4) {
+        if (shape.i < 9 && board[shape.i + 1][shape.j] !== 4) {
+            shape.i++;
+        }
+    }
+    if (board[shape.i][shape.j] === 1) {
+        score++;
+    }
+    board[shape.i][shape.j] = 2;
+    var currentTime = new Date();
+    time_elapsed = (currentTime - start_time) / 1000;
+    if (score >= 20 && time_elapsed <= 10) {
+        pac_color = "green";
+    }
+    if (score === 50) {
+        window.clearInterval(interval);
+        window.alert("Game completed");
+    } else {
+        Draw();
+    }
+}
